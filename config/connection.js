0d66cd3f2fac915eb2789e32f830fa138635c323
travis-john <travis.wl.john@gmail.com>
2020-02-15 11:52:55 -0500

    configuring connection for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 //Requiring mysql
 var mysql = require('mysql');
+let connection;
 
 //Configuring connection
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Y7'zA@5q",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+     connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "Y7'zA@5q",
+      database: "burgers_db"
+  });
+}
 
 //Connecting to database
 connection.connect(function(err) {
